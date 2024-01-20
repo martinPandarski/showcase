fb580dc6e42a5ebd5dcccb65d49ff26fa88ff4b0
martinPandarski <martinpandarski@gmail.com>
2024-01-20 20:52:36 +0200

    add gtm support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePluginRadar({
+      gtm: {
+        id: "GTM-P2VZSB3H",
+      },
+    }),
+  ],
 });
